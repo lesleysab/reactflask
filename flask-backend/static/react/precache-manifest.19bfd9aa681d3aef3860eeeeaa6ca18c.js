b38b3954e5f855d1f64143cd911683b217610cbb
lesleysab <lesleysab210@gmail.com>
2019-04-01 16:14:52 -0500

    initial commit

---
+++ flask-backend/static/react/precache-manifest.19bfd9aa681d3aef3860eeeeaa6ca18c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "450a472931a815e8e4c5",
+    "url": "/static/react/js/runtime~main.9d44af5e.js"
+  },
+  {
+    "revision": "6532b6cb111c6afa887f",
+    "url": "/static/react/js/main.cea706d6.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/react/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "6532b6cb111c6afa887f",
+    "url": "/static/react/css/main.a354e6f0.chunk.css"
+  },
+  {
+    "revision": "b49cbb1d08a4dacc045d7f4b1e152cb7",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
